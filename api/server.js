bec1adecc68ee130b0c665a60e79de47b0f30f8f
unknown <lorenzo.lgayle@gmail.com>
2022-06-06 20:18:43 -0400

    6/10 complete

--- api/server.js
+++ api/server.js
@@ -1,3 +1,70 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+const Model = require("./users/model")
+
+server.get("/", (req, res)=>{
+    console.log("received a request");
+    res.end();
+})
+
+server.post("/api/users", (req, res)=>{
+    Model.insert(req.body)
+    .then(result=>{
+        res.status(201).json(result);
+    })
+    .catch(result =>{
+        res.status(500).json(result,{message: "There was an error while saving the user to the database"})
+    })
+})
+
+server.get('/api/users', (req, res)=>{
+    Model.find()
+    .then(result => {
+        if(result == null){
+        res.status(500).json({message: "The users information could not be retrieved" })
+        return;
+    }
+    res.json(result);
+    })
+    
+    });  
+
+
+server.get('/api/users/:id', (req, res)=>{
+    Model.findById(req.params.id)
+    .then(result =>{
+        if(result == null){
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+            return;
+        }
+            res.json(result);
+        });
+    });
+
+server.delete("/api/users/:id", (req, res)=>{
+    Model.remove().then(result =>{
+            if(result == null){
+                res.status(404).json({message:  "The user with the specified ID does not exist"  })
+                return;
+            }
+                res.json(result);
+        })
+    })
+
+server.put("/api/users/:id", (req,res)=>{
+    Model.update(res.params.id, req.body)
+        .then(result =>{
+            if(result == null){
+                res.status(400).json({message: "Please provide name and bio for the user"})
+                return;
+            }
+                res.json(result);
+            });
+        });
+    
+    
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
