38c597cc2ad1810f1cf8d03977af65da86dac877
unknown <lorenzo.lgayle@gmail.com>
2022-06-06 22:09:59 -0400

    one question remaining

--- api/server.js
+++ api/server.js
@@ -4,19 +4,25 @@ const express = require('express');
 const server = express();
 const Model = require("./users/model")
 
+server.use(express.json())
+
 server.get("/", (req, res)=>{
     console.log("received a request");
     res.end();
 })
 
 server.post("/api/users", (req, res)=>{
-    Model.insert(req.body)
-    .then(result=>{
-        res.status(201).json(result);
-    })
-    .catch(result =>{
-        res.status(500).json(result,{message: "There was an error while saving the user to the database"})
-    })
+    if (!req.body.name || !req.body.bio){
+        res.status(400).json({message:"Please provide name and bio for the user"})
+    } else{
+        Model.insert(req.body)
+        .then(result =>{
+            res.status(201).json(result)
+        })
+        .catch( err=>{
+            res.status(500).json({message:"There was an error while saving the user to the database"})
+        })
+    }
 })
 
 server.get('/api/users', (req, res)=>{
@@ -44,7 +50,7 @@ server.get('/api/users/:id', (req, res)=>{
     });
 
 server.delete("/api/users/:id", (req, res)=>{
-    Model.remove().then(result =>{
+    Model.remove(req.params.id).then(result =>{
             if(result == null){
                 res.status(404).json({message:  "The user with the specified ID does not exist"  })
                 return;
@@ -54,17 +60,35 @@ server.delete("/api/users/:id", (req, res)=>{
     })
 
 server.put("/api/users/:id", (req,res)=>{
-    Model.update(res.params.id, req.body)
+    if(!req.body.name || !req.body.bio){
+        res.status(400).json({message:"Please provide name and bio for the user"})
+    }else{
+        Model.update(req.params.id, req.body)
         .then(result =>{
-            if(result == null){
-                res.status(400).json({message: "Please provide name and bio for the user"})
-                return;
-            }
-                res.json(result);
-            });
+            res.json(result);
+        })
+        .catch(result =>{
+            res.status(500).json({message:"There was an error while saving the user to the database"})
+        })
+    }
+    
         });
     
     
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
+
+// server.put("/api/users/:id", (req,res)=>{
+//     if (!req.body.name || !req.body.bio){
+//         res.status(400).json({message:"Please provide name and bio for the user"})
+//     } else{
+//         Model.update(req.body)
+//         .then(result =>{
+//             res.status(404).json({message: "The user with the specified ID does not exist"})
+//         })
+//         .catch( err=>{
+//             res.status(500).json({message:"There was an error while saving the user to the database"})
+//         })
+//     }
+// })
